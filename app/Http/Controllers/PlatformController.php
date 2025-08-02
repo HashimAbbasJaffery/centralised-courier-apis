00e28b35b761c66f13d333dae37669f8cb389324
hashimAbbasJaffery <habbas2121@outlook.com>
2025-08-02 15:16:20 -0700

    Platforms APIs

---
+++ app/Http/Controllers/PlatformController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Platform;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class PlatformController extends Controller
+{
+    public function get() {
+        $platforms = Platform::all();
+
+        return $platforms;
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            "platform" => [ "required" ]
+        ]);
+
+        $platform = Platform::create([
+            "name" => $request->platform
+        ]);
+
+        return response()->json(["message" => "Platform has been created!"]);
+    }
+    public function update(Request $request, Platform $platform) {
+        $validated = $request->validate([
+            "platform" => [ "required" ]
+        ]);
+
+        $platform = $platform->update([
+            "name" => $request->platform
+        ]);
+
+
+        return response()->json(["message" => "Platform has been updated!"]);
+    }
+
+    public function destroy(Request $request, Platform $platform) {
+        if(!$platform->exists()) {
+            throw new ModelNotFoundException("Platform not found!");
+        }
+        $platform->delete();
+        return response()->json(["message"=> "Platform has been deleted!"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\Courier\CourierController;
+use App\Http\Controllers\PlatformController;
+use App\Models\Platform;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/cities/{courier:courier_service}", [CourierController::class, "getCities"])->name("courier.cities");
-Route::get("/order/{courier:courier_service}/create", [CourierController::class, "createOrder"])->name("courier.createOrder");
-Route::get("/order/{courier:courier_service}/cancel", [CourierController::class, "cancelOrder"])->name("order.cancel");
+Route::post("/order/{courier:courier_service}/create", [CourierController::class, "createOrder"])->name("courier.createOrder");
+Route::post("/order/{courier:courier_service}/cancel", [CourierController::class, "cancelOrder"])->name("order.cancel");
 Route::get("/order/{courier:courier_service}/track", [CourierController::class, "track"])->name("order.track");
+
+Route::get("/platforms", [PlatformController::class, "get"])->name("platforms.get");
+Route::post("platform/create", [PlatformController::class, "store"])->name("platform.create");
+Route::post("platform/{platform}/update", [PlatformController::class, "update"])->name("platform.update");
+Route::post("platform/{platform}/delete", [PlatformController::class, "destroy"])->name("platform.delete");
 
 Route::get('/user', function (Request $request) {
     return $request->user();
